ffac0070793fab072aeabe9bf4d2bdd5bde45a71
emir <emirkartal00@gmail.com>
2023-12-14 13:06:40 +0300

    add types & container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { Html2canvasService } from './services/html2canvas.service';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'cropyai-task';
+export class AppComponent implements OnInit{
+	title = 'cropyai-task';
+
+	container: HTMLElement = null!;
+
+	constructor(
+		private html2canvasService: Html2canvasService,
+	) { }
+
+	ngOnInit(): void {
+		this.container = document.getElementById('container')!;
+	}
+
+	downloadImage() {
+		this.html2canvasService.downloadImage(this.container);
+	}
+
+
 }
---
+++ src/app/shared/types.ts
@@ -0,0 +1,6 @@
+export enum mimeTypes {
+    png = ".png",
+    jpg = ".jpeg",
+    heic = ".heic",
+    webp = ".webp"
+}
